bb96301f3ec8b3298bf97aa49862d4efa0e0d397
刘耀宇 <924264399@qq.com>
2026-01-26 21:07:25 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,36 @@
+
+#include <windows.h>   // 窗口
+
+
+//第一个参数是应用程序实例句柄 程序的身份证  第二个是无用兼容遗留 第三是 程序启动时外部传入的参数（比如控制台打参数）  第四个是窗口初始显示命令 还有最大化最小化隐藏窗口
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	//regiter 注册
+
+
+	//create 创建
+
+
+	//show 展示
+	return 0;
+
+
+
+}
+
+
+
+
+
+
+
+
+//int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+//{
+//
+//	return 0;
+//
+//
+//
+//}
+
