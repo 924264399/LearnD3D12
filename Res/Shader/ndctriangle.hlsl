52d1efb711aa66b6ac66449d30d631c84fc8bc39
刘耀宇 <924264399@qq.com>
2026-03-05 20:31:50 +0800

    0305   简单光照

--- Res/Shader/ndctriangle.hlsl
+++ Res/Shader/ndctriangle.hlsl
@@ -36,6 +36,8 @@ struct VSout{
 
     float4 position : SV_POSITION;
     float4 normal : NORMAL;
+    float4 texcoord : TEXCOORD0; 
+    float4 positionWS : TEXCOORD1; 
 };
 
 
@@ -53,6 +55,8 @@ VSout MainVS(VertexDate inVertexData){
 
 
     vo.normal = mul(IT_ModelMatrix,inVertexData.normal); 
+    vo.positionWS = posWS;
+    vo.texcoord = inVertexData.texcoord;
 
     //vo.normal = inVertexData.normal;
     return vo;
@@ -64,7 +68,13 @@ VSout MainVS(VertexDate inVertexData){
 half4 MainPS(VSout inPSInput) : SV_TARGET{
 
     float3 N=normalize(inPSInput.normal.xyz); 
+    float3 L = normalize(float3(1.0f,1.0f,-1.0f)); //光源方向 斜上方 
+    half3 LightColor = half3(0.1h,0.4h,0.6h); 
 
+    float3 CameraPosWS = float3(0.0f,0.0f,-5.0f); //相机位置 世界空间
+    float3 V = normalize(CameraPosWS - inPSInput.positionWS.xyz); 
+    float3 R = normalize(reflect(-L, N)); //反射向量
+    float3 H = normalize(L + V); //半程向量
 
     half3 bottomColor = half3(0.1h,0.4h,0.6h);
     half3 topColor = half3(0.7h,0.7h,0.7h);
@@ -74,13 +84,19 @@ half4 MainPS(VSout inPSInput) : SV_TARGET{
     theta/= PI; //归一化到0~1之间
     theta+=0.5f; //调整到0~1之间
 
-    half ambientIns = 1.0h; 
+    half ambientIns = 0.2h; 
 
     half3 ambientColor = lerp(bottomColor,topColor,theta) * ambientIns; 
 
-    half3 diffuseColor = half3(0.0h,0.0h,0.0h); 
+   
+    half NoL = saturate(dot(N,L)); 
+    half NoH = saturate(dot(N,H));
+     
 
-    half3 specularColor = half3(0.0h,0.0h,0.0h); 
+
+    half3 diffuseColor = LightColor * NoL; 
+
+    half3 specularColor = pow(NoH,16.0h) * LightColor ; 
 
     half3 surfaceColor = ambientColor + diffuseColor + specularColor;  
 
